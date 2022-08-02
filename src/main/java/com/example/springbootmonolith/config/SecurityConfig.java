88de7d90146216cfc9e3b26483cd14bb1e5c8325
sanand100 <shraddhaanand1@gmail.com>
2022-08-02 19:02:00 -0400

    working on cors

--- src/main/java/com/example/springbootmonolith/config/SecurityConfig.java
+++ src/main/java/com/example/springbootmonolith/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/blogs").allowedOrigins("https://movingco.netlify.app/");
+                registry.addMapping("/**").allowedOrigins("https://movingco.netlify.app/");
             }
         };
     }
